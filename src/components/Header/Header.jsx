8d5b471878008302a5665696bc505164f7f78df8
ViniVanC <vincentvan0302@gmail.com>
2023-10-05 15:21:12 +0300

    added styles to Header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import { Logo } from "../Logo/Logo";
 
+import "./Header.scss";
+
 export const Header = () => {
   return (
     <header className="header">
-      <Logo />
+      <Logo size={40} />
     </header>
   );
 };
