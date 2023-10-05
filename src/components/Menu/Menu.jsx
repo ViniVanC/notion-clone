67e175b102630ef56e72d9e9af2c413faa4597f6
ViniVanC <vincentvan0302@gmail.com>
2023-10-05 16:08:09 +0300

    created structure menu

--- src/components/Menu/Menu.jsx
+++ src/components/Menu/Menu.jsx
@@ -1,5 +1,22 @@
 import React from "react";
+import { Button } from "../Button/Button";
+
+import "./Menu.scss";
 
 export const Menu = () => {
-  return <nav>nav</nav>;
+  return (
+    <nav className="menu">
+      <header className="menu__header">
+        <Button className={"closeBtn"} />
+      </header>
+      <Button />
+
+      <h2 className="menu__title">Notions list</h2>
+      <ul className="menu__list menu-list">
+        <li className="menu-list__item">
+          <a href="#">Note 1</a>
+        </li>
+      </ul>
+    </nav>
+  );
 };
