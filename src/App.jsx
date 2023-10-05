96d44b4613e7fcbe8cde515a88459614b3308db0
ViniVanC <vincentvan0302@gmail.com>
2023-10-05 15:06:20 +0300

    added components to App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,17 @@
+import { Header } from "./components/Header/Header";
+import { Menu } from "./components/Menu/Menu";
+import { Note } from "./components/Note/Note";
+
 function App() {
-  return <>hi </>;
+  return (
+    <>
+      <Header />
+      <section>
+        <Menu />
+        <Note />
+      </section>
+    </>
+  );
 }
 
 export default App;
