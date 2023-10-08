38555dc9afe1921eeb80cbaf7ca9f0129c8379ec
ViniVanC <vincentvan0302@gmail.com>
2023-10-08 22:05:19 +0300

    created Register & LogIn pages

--- src/Pages/LoginRegister/LogIn.jsx
+++ src/Pages/LoginRegister/LogIn.jsx
@@ -1,10 +1,22 @@
 import React from "react";
 import { Container } from "../../components/Container/Container";
+import { Link } from "react-router-dom";
 
 export const LogIn = () => {
   return (
-    <div>
-      <Container></Container>
-    </div>
+    <section className="section">
+      <Container>
+        <div className="section__inner">
+          <form className="form">
+            <input type="text" placeholder="Email" />
+            <input type="password" placeholder="Password" />
+            <button type="submit">Log in</button>
+            <p>
+              Don't have an account yet <Link to={"/register"}>Register</Link>
+            </p>
+          </form>
+        </div>
+      </Container>
+    </section>
   );
 };
--- src/Pages/LoginRegister/Register.jsx
+++ src/Pages/LoginRegister/Register.jsx
@@ -1,5 +1,23 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import { Container } from "../../components/Container/Container";
 
 export const Register = () => {
-  return <div>Register</div>;
+  return (
+    <section className="section">
+      <Container>
+        <div className="section__inner">
+          <form className="form">
+            <input type="text" placeholder="User name" />
+            <input type="text" placeholder="Email" />
+            <input type="password" placeholder="Password" />
+            <button type="submit">Register</button>
+            <p>
+              Already have an account <Link to={"/login"}>Log in</Link>
+            </p>
+          </form>
+        </div>
+      </Container>
+    </section>
+  );
 };
